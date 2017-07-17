f602e2dbd1be664626c0918359a2171f5ecf6717
Pancakes13 <neil.llenes@gmail.com>
2017-07-17 18:00:16 +0800

    Created OJT SEO Page
    Created edit, insert and select for the OJT SEO page!

---
+++ editFunctions/editDailyTaskOJTSeo.php
@@ -0,0 +1,25 @@
+<?php
+    require('../sql_connect.php');
+    $postdata = file_get_contents("php://input");
+    $request = json_decode($postdata, true);
+    if(count($request>0)){
+        $id = $request['id'];
+        $comment = $request['comment'];
+        $site = $request['site'];
+        $schema = $request['schema'];
+        $competitor = $request['competitor'];
+        $relationship = $request['relationship'];
+        $misc = $request['misc'];
+
+        $query = "UPDATE `ojt_seo_tracker` 
+        SET `comment` = '$comment', 
+        `site_audit` = '$site',
+        `schema_markup` = '$schema',
+        `competitor_backlink_analysis` = '$competitor',
+        `relationship_link_research` = '$relationship',
+        `misc` = '$misc'
+        WHERE `ojt_seo_id` = $id";
+
+        $result = mysqli_query($mysqli, $query);
+    }
+?>
