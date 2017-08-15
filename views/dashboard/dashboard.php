123d855bd187d2608545eb6ddf17e7946939717d
xTJz <tjlerias4@gmail.com>
2017-08-15 18:09:07 +0800

    added css

--- views/dashboard/dashboard.php
+++ views/dashboard/dashboard.php
@@ -101,6 +101,19 @@
       .calendar.table-bordered > tbody > tr > th{
           border:1px solid black;
       }
+
+      .dropdown-content {
+          display: none;
+          position: absolute;
+          background-color: #f9f9f9;
+          min-width: 160px;
+          box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+          z-index: 1;
+      }
+
+      .name_result:hover {
+          color: blue;
+      }
     </style>
   
   </head>
