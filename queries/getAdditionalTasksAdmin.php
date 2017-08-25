4b521fa0e619384c677dcd838800c4d276aa7df0
Pancakes13 <neil.llenes@gmail.com>
2017-08-25 14:22:20 +0800

    Fixed bug with additional tasks

---
+++ queries/getAdditionalTasksAdmin.php
@@ -0,0 +1,26 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+require("../functions/sql_connect.php");
+session_start();
+
+$val = $_GET['user_id'];
+$result = $mysqli->query("SELECT *
+FROM `additional_task`
+WHERE `user_id` = $val");
+
+$outp = "";
+while($rs = $result->fetch_array(MYSQLI_ASSOC)) {
+    if ($outp != "") {
+        $outp .= ",";
+    }
+    $outp .= '{"AdditionalTaskId":"'  . $rs["additional_task_id"] . '",';
+    $outp .= '"Name":"'  . $rs["name"] . '",';
+    $outp .= '"Type":"'  . $rs["type"] . '",';
+    $outp .= '"UserId":"'   . $rs["user_id"]        . '"}';
+}
+$outp ='{"records":['.$outp.']}';
+$mysqli->close();
+
+echo($outp);
+?>
--- queries/getMyDailyTrackerTodayAdditionalTaskTracker.php
+++ queries/getMyDailyTrackerTodayAdditionalTaskTracker.php
@@ -2,11 +2,15 @@
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 require("../functions/sql_connect.php");
+$postdata = file_get_contents("php://input");
+session_start();
 
-$result = $mysqli->query("SELECT `t`.`name`, `a`.`additional_task_tracker_id`, `a`.`task`
+$val = $_SESSION['user_id'];
+
+$result = $mysqli->query("SELECT `t`.`name`, `a`.`additional_task_tracker_id`, `a`.`task`, `a`.`entry_time`
 FROM `additional_task_tracker` `a`
 INNER JOIN `additional_task` `t`
-ON `t`.`additional_task_id` = `a`.`additional_task_id` AND `a`.`track_date` = CURDATE()");
+ON `t`.`additional_task_id` = `a`.`additional_task_id` AND `a`.`track_date` = CURDATE() AND `t`.`user_id`=$val");
 
 $outp = "";
 while($rs = $result->fetch_array(MYSQLI_ASSOC)) {
@@ -15,6 +19,7 @@ while($rs = $result->fetch_array(MYSQLI_ASSOC)) {
     }
     $outp .= '{"AdditionalTaskTrackerId":"'  . $rs["additional_task_tracker_id"] . '",';
     $outp .= '"Name":"'   . $rs["name"]        . '",';
+    $outp .= '"Time":"'   . $rs["entry_time"]        . '",';
     $outp .= '"Task":"'   . $rs["task"]        . '"}';
 }
 $outp ='{"records":['.$outp.']}';
