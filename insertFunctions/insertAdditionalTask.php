e4af820b436e844fe05d5e83a1e9afa971e0f941
Pancakes13 <neil.llenes@gmail.com>
2017-09-22 14:21:03 +0800

    Created a function for insert additional task tracker

--- insertFunctions/insertAdditionalTask.php
+++ insertFunctions/insertAdditionalTask.php
@@ -11,6 +11,13 @@
     $query = "INSERT INTO `additional_task`(`name`, `type`, `user_id`) 
         VALUES ('$name', '$type', $userId)";
         $result = mysqli_query($mysqli, $query);
+    $last_id = $mysqli->insert_id;
+    echo "New record created successfully. Last inserted ID is: " . $last_id;
+
+    $query2 = "INSERT INTO `additional_task_tracker`
+    (`task`, `track_date`, `entry_time`, `additional_task_id`) 
+    VALUES (' ', CURDATE(), NOW(), $last_id)";
+    $result2 = mysqli_query($mysqli, $query2);
   }else{
       echo "error";
   }
